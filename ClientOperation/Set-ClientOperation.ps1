741e5be673279541bd56295ad56584dc0154e945
MaikKoster <MaikKoster@users.noreply.github.com>
2015-12-08 11:44:20 +0100

    Added "SupportsShouldProcess" to the Main function so @PSBoundParameter can properly forward this parameter.

--- ClientOperation/Set-ClientOperation.ps1
+++ ClientOperation/Set-ClientOperation.ps1
@@ -127,7 +127,7 @@ Begin {
     ###############################################################################
     function Main {
 
-        [CmdLetBinding()]
+        [CmdLetBinding(SupportsShouldProcess)]
         PARAM(
             [string]$Start,
             [switch]$Cancel,
