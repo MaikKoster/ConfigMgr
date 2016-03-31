d1cd9aa31468493f395caf6c59894f8f28f7d149
MaikKoster <MaikKoster@users.noreply.github.com>
2016-03-31 14:04:15 +0200

    Prepared Export-TaskSequence.ps1 for public publish.

--- TaskSequence/Export-TaskSequence.ps1
+++ TaskSequence/Export-TaskSequence.ps1
@@ -1,33 +1,54 @@
 #Requires -Version 3.0
+#Requires -Modules ConfigMgr
 
 <#
     .SYNOPSIS
-        Exports a Task Sequence to a XML file
+        Exports a Task Sequence as an XML file
 
     .DESCRIPTION 
-        Exports a Task Sequence to a XML file as used by ConfigMgr 2007 and older.
+        Exports a Task Sequence as an XML file as used by ConfigMgr 2007 and older.
         
         ConfigMgr 2012 and above use a different format for the default export via the ConfigMgr console
         which consists of several files and folders compressed into an archive which can also contain copies 
         of the referenced packages. This script creates only a XML file which can only get imported back 
         using the corresponding "Import-TaskSequence.ps1" script.
 
+    .EXAMPLE
+        .\Export-TaskSequence.ps1 -Path "%Temp%\Export" -ID "TST00001"
+    
+        Export a Task Sequence to a subfolder with incrementing suffix.
+
+    .EXAMPLE
+        .\Export-TaskSequence.ps1 -Path "%Temp%\Export" -ID "TST00001" -Filename "#ID.xml" -Force
+    
+        Export a Task Sequence and overwrite the last exported file.
+
+    .EXAMPLE
+        .\Export-TaskSequence.ps1 -Path "%Temp%\Export" -ID "TST00001", "TST00002", "TST00003" -NoProgress
+    
+        Export several Task Sequences without progress information. E.g. as regular backup task.
+
+    .EXAMPLE
+        .\Export-TaskSequence.ps1 -Path "%Temp%\Export" -ID "TST00001" -ProviderServer TSTCM01 -SiteCode TST -Credentials (Get-Credential)
+        
+        Export a Task Sequence using different credentials and Provider server settings
+
     .LINK
         http://maikkoster.com/
+        https://github.com/MaikKoster/ConfigMgr/blob/master/TaskSequence/Export-TaskSequence.ps1
 
     .NOTES
-        Copyright (c) 2015 Maik Koster
+        Copyright (c) 2016 Maik Koster
 
         Author:  Maik Koster
         Version: 1.0
-        Date:    15.03.2016
+        Date:    31.03.2016
 
         Version History:
-            1.0 - 15.03.2016 - Published script
+            1.0 - 31.03.2016 - Published script
             
-
 #>
-[CmdLetBinding(SupportsShouldProcess)]
+[CmdLetBinding(SupportsShouldProcess,DefaultParameterSetName="ID")]
 PARAM (
     # Specifies the path for the exported Task Sequence xml file.
     [Parameter(Mandatory, ParameterSetName="ID")]
@@ -49,7 +70,7 @@ PARAM (
     # Use either ID or Name to select the Task Sequence.
     [Parameter(Mandatory, ParameterSetName="ID")]
     [ValidateNotNullOrEmpty()]
-    [Alias("PackageID")]
+    [Alias("PackageID", "TaskSequenceID")]
     [string[]]$ID,
 
     # Specifies the Task Sequence Name.
@@ -65,9 +86,11 @@ PARAM (
     # On default, any existing file will not be overwritten.
     [switch]$Force,
 
-    # Disables the Progress output. 
-    # Usefull for automated Export.
-    [switch]$NoProgress,
+    # Enables the Progress output. 
+    [switch]$ShowProgress,
+
+    # Specifies if the script should pass through the path to the export file
+    [switch]$PassThru,
 
     # Specifies the ConfigMgr Provider Server name. 
     # If no value is specified, the script assumes to be executed on the Site Server.
@@ -82,7 +105,6 @@ PARAM (
     [System.Management.Automation.Credential()]$Credential = [System.Management.Automation.PSCredential]::Empty
 )
 
-
 Process {
 
     ###############################################################################
@@ -93,7 +115,7 @@ Process {
     if ($MyInvocation.InvocationName -ne '.') {
 
         # Create a connection to the ConfigMgr Provider Server
-        if (!($NoProgress.IsPresent)) {Write-Progress -Id 1 -Activity "Exporting Task Sequences .." -Status "Connecting to ConfigMgr ProviderServer" -PercentComplete 0}
+        if (!($ShowProgress.IsPresent)) {Write-Progress -Id 1 -Activity "Exporting Task Sequences .." -Status "Connecting to ConfigMgr ProviderServer" -PercentComplete 0}
         $ConnParams = @{ServerName = $ProviderServer;SiteCode = $SiteCode;}
 
         if ($PSBoundParameters["Credential"]) {$connParams.Credential = $Credential}
@@ -103,23 +125,24 @@ Process {
         # Prepare parameters for splatting
         $ExportParams = @{Path = $Path; Filename = $Filename}
         if ($KeepSecretInformation.IsPresent) {$ExportParams.KeepSecretInformation = $true}
-        if ($NoProgress.IsPresent) {$ExportParams.NoProgress = $true}
         if ($Force.IsPresent) {$ExportParams.Force = $true}
+        if ($ShowProgress.IsPresent) {$ExportParams.ShowProgress = $true}
+        if ($PassThru.IsPresent) {$ExportParams.PassThru = $true}
 
         # Start export based on parameter set
         $Count = 0        
         switch ($PSCmdLet.ParameterSetName) {
             "ID" {
                     ForEach ($TSID In $ID) {
-                        if (!($NoProgress.IsPresent)) {Write-Progress -Id 1 -Activity "Exporting Task Sequences .." -Status "Processing Task Sequence $TSID .." -PercentComplete ($Count / $ID.Count * 100)}
+                        Set-Progress -ShowProgress:($ShowProgress.IsPresent) -Activity "Exporting Task Sequences .." -Status "Processing Task Sequence $TSID .." -TotalSteps $ID.Count -Step $Count
                         Start-Export -ID $TSID @ExportParams
                         $Count++
                     }
                 }
             "Name" {
                     Foreach ($TSName In $Name) {
-                        if (!($NoProgress.IsPresent)) {Write-Progress -Id 1 -Activity "Exporting Task Sequences .." -Status "Processing Task Sequence $TSName .." -PercentComplete ($Count / $Name.Count * 100)}
-                        Start-Export -Path $Path -Name $TSName -KeepSecretInformation:$KeepSecretInformation -NoProgress:$NoProgress
+                        Set-Progress -ShowProgress:($ShowProgress.IsPresent) -Activity "Exporting Task Sequences .." -Status "Processing Task Sequence $TSName .." -TotalSteps $Name.Count -Step $Count
+                        Start-Export -Name $TSName @ExportParams
                         $Count++
                     }
                 }
@@ -130,11 +153,10 @@ Process {
 Begin {
 
     Set-StrictMode -Version Latest
-
-    # Import ConfigMgr Module
-    if (-not (Get-Module ConfigMgr)) {Import-Module ConfigMgr}
     
-    function Start-Export {
+    # Starts the Export process. 
+    # Moved to separate function to properly test the execution using Pester.
+    Function Start-Export {
         [CmdLetBinding(SupportsShouldProcess)]
         PARAM(
             # Specifies the path for the exported Task Sequence xml file.
@@ -158,7 +180,7 @@ Begin {
             # Use either ID or Name to select the Task Sequence
             [Parameter(Mandatory,ParameterSetName="ID",ValueFromPipelineByPropertyName)]
             [ValidateNotNullOrEmpty()]
-            [Alias("PackageID")]
+            [Alias("PackageID", "TaskSequenceID")]
             [string]$ID,
 
             # Specifies the Task Sequence Name.
@@ -177,15 +199,20 @@ Begin {
             # Specifies the ID of the parent progress 
             [int]$ParentProgressID = -1,
 
-            # Disables the Progress output. 
-            # Usefull for automated Export.
-            [switch]$NoProgress
+            # Enables the Progress output. 
+            [switch]$ShowProgress,
+
+            # Specifies if the script should pass through the path to the export file
+            [switch]$PassThru
         )
 
         Begin {
-            $ProgressParams = @{}
+            $ProgressParams = @{
+                TotalSteps = 6
+                ShowProgress = $ShowProgress.IsPresent
+            }
 
-            if (($ParentProgressID -ne $null) -and ($ParentProgressID -gt $null)) {
+            if (($ParentProgressID -ne $null) -and ($ParentProgressID -gt 0)) {
                 $ProgressParams.ParentId = $ParentProgressID
                 $ProgressParams.Id = $ParentProgressID + 1
             }
@@ -194,30 +221,32 @@ Begin {
         Process {
             # Get Task Sequence Package
             $ProgressParams.Activity = "Exporting Task Sequence $ID$Name"
-            if (!($NoProgress.IsPresent)) {Write-Progress @ProgressParams -Status "Getting Task Sequence Package" -PercentComplete (100/6)}
+            Set-Progress @ProgressParams -Status "Getting Task Sequence Package" -Step 1
             if (!([string]::IsNullOrEmpty("ID"))) {
-                $TaskSequencePackage = Get-TaskSequencePackage -ID $ID
+                Write-Verbose "Start Export Process for Task Sequence Package $ID."
+                $TaskSequencePackage = Get-TaskSequencePackage -ID $ID -IncludeLazy
             } else {
-                $TaskSequencePackage = Get-TaskSequencePackage -Name $Name
+                Write-Verbose "Start Export Process for Task Sequence Package '$Name'."
+                $TaskSequencePackage = Get-TaskSequencePackage -Name $Name -IncludeLazy
             }
 
             # Get Task Sequence
             if ($TaskSequencePackage -ne $null){
-                $ProgressParams.Activity = "Exporting Task Sequence ""$($TaskSequencePackage.Name)"" ($($TaskSequencePackage.PackageID))"
+                $ProgressParams.Activity = "Exporting Task Sequence '$($TaskSequencePackage.Name)' ($($TaskSequencePackage.PackageID))"
                 #if (!($NoProgress.IsPresent) -and ($ParentProgressID -gt 0)) {Write-Progress -Id $ParentProgressID -Status "Processing Task Sequence $($TaskSequencePackage.Name) ($($TaskSequencePackage.PackageID)) .."}
-                if (!($NoProgress.IsPresent)) {Write-Progress @ProgressParams -Status "Getting Task Sequence from Task Sequence Package" -PercentComplete (100/6*2)}
+                Set-Progress @ProgressParams -Status "Getting Task Sequence from Task Sequence Package" -Step 2
                 $TaskSequence = Get-TaskSequence -TaskSequencePackage $TaskSequencePackage
             }
 
             if ($TaskSequence -ne $null) {
                 # Convert to xml
-                if (!($NoProgress.IsPresent)) {Write-Progress @ProgressParams -Status "Converting Task Sequence to XML" -PercentComplete (100/6*3)}
+                Set-Progress @ProgressParams -Status "Converting Task Sequence to XML" -Step 3
                 $TaskSequenceXML = [xml](Convert-TaskSequenceToXML -TaskSequence $TaskSequence -KeepSecretInformation:$KeepSecretInformation)
 
                 if ($TaskSequenceXML -ne $null) {
-                    if (!($NoProgress.IsPresent)) {Write-Progress @ProgressParams -Status "Adding Package properties to XML" -PercentComplete (100/6*4)}
+                    Set-Progress @ProgressParams -Status "Adding Package properties to XML" -Step 4
                     $Result = Add-PackageProperties -TaskSequencePackage $TaskSequencePackage -TaskSequenceXML $TaskSequenceXML
-                    if (!($NoProgress.IsPresent)) {Write-Progress @ProgressParams -Status "Saving xml file to $Path" -PercentComplete (100/6*5)}
+                    Set-Progress @ProgressParams -Status "Saving xml file to $Path" -Step 5
 
                     $TSFilename = Get-Filename -Path $Path -Filename $Filename -PackageID $TaskSequencePackage.PackageID -PackageName $TaskSequencePackage.Name
 
@@ -227,16 +256,24 @@ Begin {
                     if (!(Test-path($ParentPath))) { New-Item -ItemType Directory -Force -Path $ParentPath | Out-Null}
 
                     # Save XML file
-                    $Result.Save($FullName)
-                    if (!($NoProgress.IsPresent)) { Write-Progress @ProgressParams -Status "Done" -PercentComplete 100}
+                    if ($PSCmdLet.ShouldProcess("Write Task Sequence xml to file '$FullName'.", "Write XML File")) {
+                        Write-Verbose "Write Task Sequence xml to file '$Fullname'."
+                        if ((Test-Path($FullName)) -and (-not ($Force.IsPresent))) {
+                            Write-Warning "File '$Fullname' exists already and Force isn't set. Won't overwrite existing file."
+                        } else {
+                            $Result.Save($FullName)
+                        }
+                        Set-Progress @ProgressParams -Status "Done" -Step 6
+                    }
+                    if ($PassThru.IsPresent) {$FullName}
                 }
             }
         }
     }
 
     # Extends the Task Sequence xml document with some default properties of the Task Sequence Package
-    function Add-PackageProperties {
-        [CmdLetBinding()]
+    Function Add-PackageProperties {
+        [CmdLetBinding(SupportsShouldProcess)]
         PARAM (
             # Specifies the Task Sequence Package. 
             [Parameter(Mandatory)]
@@ -249,42 +286,39 @@ Begin {
             [xml]$TaskSequenceXML
         )
 
-        # Some of the properties we use are lazy properties, so we need to get a different instance of the TS Package
-        $TaskSequencePackageWMI = Get-TaskSequencePackage -ID ($TaskSequencePackage.PackageID) -IncludeLazyProperties
-
         # Generate a new xml file
         # To ease the job, lets create a string first
 
         $Output = "<SmsTaskSequencePackage><BootImageID>"
-        $Output += $TaskSequencePackageWMI.BootImageID
+        $Output += $TaskSequencePackage.BootImageID
         $Output += "</BootImageID><Category>"
-        $Output += $TaskSequencePackageWMI.Category
+        $Output += $TaskSequencePackage.Category
         $Output += "</Category><DependentProgram>"
-        $Output += $TaskSequencePackageWMI.DependentProgram
+        $Output += $TaskSequencePackage.DependentProgram
         $Output += "</DependentProgram><Description>"
-        $Output += $TaskSequencePackageWMI.Description
+        $Output += $TaskSequencePackage.Description
         $Output += "</Description><Duration>"
-        $Output += $TaskSequencePackageWMI.Duration
+        $Output += $TaskSequencePackage.Duration
         $Output += "</Duration><Name>"
-        $Output += $TaskSequencePackageWMI.Name
+        $Output += $TaskSequencePackage.Name
         $Output += "</Name><ProgramFlags>"
-        $Output += $TaskSequencePackageWMI.ProgramFlags
+        $Output += $TaskSequencePackage.ProgramFlags
         $Output += "</ProgramFlags><SequenceData>"
         $Output += $TaskSequenceXML.sequence.OuterXml
         $Output += "</SequenceData><SourceDate>"
-        $SourceDate = [Management.ManagementDateTimeConverter]::ToDateTime($TaskSequencePackageWMI.SourceDate).ToString("yyy-MM-ddThh:mm:ss")
-        $Output += $SourceDate
+        $Output += $TaskSequencePackage.SourceDate.ToString("yyyy-MM-ddThh:mm:ss")
         $Output += "</SourceDate><SupportedOperatingSystems>"
+        #TODO Implement support for SupportedOperatingSystems
         #$Output += $TaskSequencePackageWMI.SupportedOperatingSystems
         $Output += "</SupportedOperatingSystems><IconSize>"
-        $Output += $TaskSequencePackageWMI.IconSize
+        $Output += $TaskSequencePackage.IconSize
         $Output += "</IconSize></SmsTaskSequencePackage>"
 
         return [xml]$Output
     }
 
     # Generates a file name for the Task Sequence xml file
-    function Get-Filename {
+    Function Get-Filename {
         [CmdLetBinding()]
         PARAM(
             # Specifies the path for the exported Task Sequence xml file.
@@ -319,7 +353,8 @@ Begin {
             }
 
             # Get files that match the pattern
-            $TSFiles = @(Get-ChildItem -Path $Path -Filter $TSFilename.Replace($TSFilename.Substring($StartPos,$ZeroCount + 1), "*") -ErrorAction SilentlyContinue)
+            $Pattern = $TSFilename.Replace($TSFilename.Substring($StartPos,$ZeroCount + 1), "*")
+            $TSFiles = @(Get-ChildItem -Path $Path -Filter $Pattern -ErrorAction SilentlyContinue)
 
             if ($TSFiles -ne $null) {
                 $TSFileCount = $TSFiles.Count
@@ -332,9 +367,44 @@ Begin {
             # escape certain characters for formatting
             $TSFilename = $TSFilename.Replace("0.", "0'.'").Replace("\","'\'").Replace("#0", "0")
             $TSFilename = "{0:$TSFilename}" -f $TSFileCount
-
         }
 
         $TSFilename
+    }
+
+    # Wraps the Write-Progress CmdLet
+    Function Set-Progress {
+        [CmdLetBinding()]
+        PARAM(
+            # Specifies the current ID of the Progress output
+            [int]$ID = 1,
+
+            # Specifies the Parent ID of the Progress output
+            [int]$ParentID = -1,
+
+            # Specifies the total amount of steps
+            [int]$TotalSteps,
+
+            # Specifies the current step
+            [int]$Step = 1,
+
+            # Specifies the current status
+            [string]$Status,
+
+            # Specifies the current Activity
+            [string]$Activity,
+
+            # Specifies if Progress shall be shown
+            [bool]$ShowProgress = $False
+        )
+
+        if ($ShowProgress) {
+            $PercentComplete = (100/$TotalSteps*$Step)
+            if ($ParentID -gt 0) {
+                Write-Progress -ParentId $ParentID -Id $ID -Status $Status -Activity $Activity -PercentComplete $PercentComplete
+            } else {
+                Write-Progress -Id $ID -Status $Status -Activity $Activity -PercentComplete $PercentComplete
+            }
+        }
     }
 }
